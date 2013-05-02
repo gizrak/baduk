39263e17530b8d3522a4ae4d67fa37ef9af5389a
midnight <midnight@midnight-P560.(none)>
2013-05-02 14:27:09 +0900

    add sample url

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+import jinja2
 import webapp2
 
-class MainPage(webapp2.RequestHandler):
+# Set to true if we want to have our webapp print stack traces, etc
+_DEBUG = True
 
-	def get(self):
-		self.response.headers['Content-Type'] = 'text/plain'
+class HomePage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello, webapp2 World!')
 
-app = webapp2.WSGIApplication([('/', MainPage)],
-                              debug=True)
+class AppsPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Hello, webapp2 World!')
+
+app = webapp2.WSGIApplication([
+                              ('/', HomePage), 
+                              ('/apps', AppsPage)
+                              ],
+                            debug=_DEBUG)
