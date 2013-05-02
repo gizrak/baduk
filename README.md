9fa9caf0ac4f82b4e0f21c0083b0655a352b41f3
midnight <midnight@midnight-P560.(none)>
2013-05-02 13:55:05 +0900

    initial revision

---
+++ README.md
@@ -0,0 +1,13 @@
+badook, a web based badook(go) game
+
+http://code.google.com/p/badook/
+
+see AUTHORS file for authors
+
+-----
+
+badook contains code from these projects:
+
+jQuery JavaScript Library v1.8.0
+http://jquery.com/
+Copyright (c) 2009 John Resig
---
+++ legacy/BoardLoadServlet.java
@@ -0,0 +1,14 @@
+package com.gizrak.badook;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class BoardLoadServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Board Load...");
+	}
+}
