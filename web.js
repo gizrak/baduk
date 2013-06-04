ef2330039d0b11e6adb659ee0fc3b5d9528b4104
Hwang Byungjun <doodyfree@gmail.com>
2013-06-04 19:04:07 +0900

    app.engine ejs

--- web.js
+++ web.js
@@ -21,6 +21,7 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+app.engine('html', require('ejs').renderFile);
 
 // development only
 if ('development' == app.get('env')) {
