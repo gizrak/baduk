69462c63e5a00bf12469db46979bb91a721d1bc7
Ted Hwang <ted.hwang@linecorp.com>
2020-07-02 21:42:53 +0900

    Upgrade express

--- web.js
+++ web.js
@@ -7,23 +7,25 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var methodOverride = require('method-override');
+var errorHandler = require('errorhandler');
 
 var app = express();
+
+// parse urlencoded request bodies into req.body
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
 
 // all environments
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
+app.use(methodOverride());
 app.use(express.static(path.join(__dirname, 'public')));
 app.engine('html', require('ejs').renderFile);
 
 // development only
 if ('development' == app.get('env')) {
-	app.use(express.errorHandler());
+	app.use(errorHandler());
 }
 
 app.get('/', routes.index);
@@ -33,3 +35,4 @@ var port = process.env.PORT || 5000;
 http.createServer(app).listen(port, function() {
 	console.log('Express server listening on port ' + port);
 });
+
